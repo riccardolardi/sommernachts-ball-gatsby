efe5d7bac46f7bcacea548bd419de6b6528f6086
Riccardo Lardi <hello@riccardolardi.com>
2020-04-03 13:59:04 +0200

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+		{
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `127.0.0.1:8888`,
+        useACF: true,
+        verboseOutput: false,
+        protocol: `http`,
+        hostingWPCOM: false
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+		query actQuery {
+		  allWordpressWpAct {
+		    nodes {
+		      title
+		      acf {
+		        description
+		        media {
+		          vimeo
+		          youtube
+		          image {
+		            localFile {
+		              childImageSharp {
+		                fixed(width: 1440) {
+		                  src
+		                }
+		              }
+		            }
+		          }
+		          type
+		        }
+		      }
+		    }
+		  }
+		}
+  `)
+
+  return <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+}
 
 export default IndexPage
