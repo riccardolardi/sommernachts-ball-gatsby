624497f6126f8da8fa19879004db4bea67ab205c
Riccardo Lardi <hello@riccardolardi.com>
2022-04-08 18:53:59 +0200

    yarn upgrade, fix breaking bug

--- src/components/app.js
+++ src/components/app.js
@@ -7,45 +7,43 @@ import easyScroll from "easy-scroll"
 import Three from "./three"
 import Main from "./main"
 import Navi from "./navi"
+import logoSrcB from "../assets/migros_b.svg"
+import logoSrcW from "../assets/migros_w.svg"
 import "../styles/app.scss"
 
 let mainEl
 const useBreakpoint = createBreakpoint({
-	mobile: 767, 
-	other: 768
+	mobile: 767,
+	other: 768,
 })
 
-const logoSrcB = require('../assets/migros_b.svg')
-const logoSrcW = require('../assets/migros_w.svg')
-
 const App = (props) => {
-
-  const newsData = props.data.wpgraphql.newsItems.nodes[0]
-  const infoData = props.data.wpgraphql.infos.nodes[0]
-  const lineupData = props.data.wpgraphql.lineups.nodes[0]
-  const galleryData = props.data.wpgraphql.galleries.nodes[0]
-  const newsletterData = props.data.wpgraphql.newsletters.nodes[0]
-  const contactData = props.data.wpgraphql.contacts.nodes[0]
+	const newsData = props.data.wpgraphql.newsItems.nodes[0]
+	const infoData = props.data.wpgraphql.infos.nodes[0]
+	const lineupData = props.data.wpgraphql.lineups.nodes[0]
+	const galleryData = props.data.wpgraphql.galleries.nodes[0]
+	const newsletterData = props.data.wpgraphql.newsletters.nodes[0]
+	const contactData = props.data.wpgraphql.contacts.nodes[0]
 
 	const [wp, setWp] = React.useState(null)
 	const [prevWp, setPrevWp] = React.useState(null)
 	const [isIntro, setIsIntro] = React.useState(false)
 	const [inverted, setInverted] = React.useState(true)
 	const [mobileNavOpen, setMobileNavOpen] = React.useState(false)
-	const isMobile = useBreakpoint() === 'mobile'
+	const isMobile = useBreakpoint() === "mobile"
 
 	React.useEffect(() => {
 		// setInverted(wp > 2 && wp < 9)
 		setMobileNavOpen(false)
-		setIsIntro(wp <= 2 || wp === 9)
+		setIsIntro(wp <= 2 || wp === 9)
 	}, [wp])
 
 	React.useLayoutEffect(() => {
-		mainEl = document.querySelector('main#main')
+		mainEl = document.querySelector("main#main")
 	}, [])
 
 	function jumpTo(newWp, time) {
-		const targetEl = document.querySelectorAll('[data-wp="'+newWp+'"]')[0]
+		const targetEl = document.querySelectorAll('[data-wp="' + newWp + '"]')[0]
 		const scrollEl = mainEl
 		if (time === 0) {
 			setMobileNavOpen(false)
@@ -53,11 +51,11 @@ const App = (props) => {
 			return
 		}
 		easyScroll({
-		  'scrollableDomEle': scrollEl,
-		  'direction': 'bottom',
-		  'duration': time,
-		  'easingPreset': 'easeOutQuad',
-		  'scrollAmount': targetEl.offsetTop - scrollEl.scrollTop
+			scrollableDomEle: scrollEl,
+			direction: "bottom",
+			duration: time,
+			easingPreset: "easeOutQuad",
+			scrollAmount: targetEl.offsetTop - scrollEl.scrollTop,
 		})
 	}
 
@@ -67,73 +65,98 @@ const App = (props) => {
 	}
 
 	const classes = Classnames({
-		'inverted': inverted,
-		'mobile-nav-open': mobileNavOpen,
-		'isIE': isIE
+		inverted: inverted,
+		"mobile-nav-open": mobileNavOpen,
+		isIE: isIE,
 	})
 
-  return (
-    <div id="app" className={classes}>
-    	{isIE ? <React.Fragment>
-	      <h1>Ihr Browser (Internet Explorer) ist leider veraltet.</h1>
-	      <h2>Diese Homepage kann so nicht angezeigt werden.</h2><br/>
-	      <p>Bitte installieren Sie einen aktuellen Browser wie zB. <a href="https://www.google.com/chrome/">Google Chrome</a> oder <a href="https://www.mozilla.org/de/firefox/new/">Firefox</a>.</p>
-	      <p>Mehr Informationen darüber wieso man keine veralteten Browser benutzen sollte: <a href="https://www.browser-update.org/de/update.html">hier klicken</a></p>
-    	</React.Fragment> : 
-    	<React.Fragment>
-		    {/*<Three show={isIntro || mobileNavOpen} />*/}
-		    <header className={`text-logo front 
-		    	${!isIntro && !isMobile || mobileNavOpen ? 'show' : 'hide'}`}>
-		      <div className="row">
-		        <span className="left suffix-slash">Sommer</span>
-		        <span className="right">25.</span>
-		      </div>
-		      <div className="row">
-		        <span className="left suffix-slash">Nachts</span>
-		        <span className="right">Juni</span>
-		      </div>
-		      <div className="row">
-		        <span className="left suffix-slash">Ball</span>
-		        <span className="right">2022</span>
-		      </div>
-		      <div className="row instagram-link">
-		      	<a href="https://www.instagram.com/sommernachtsballzuerich" target="_blank" rel="noopener noreferrer">
-		      		<InstagramIcon className="icon" />
-		      	</a>
-		      </div>
-		    </header>
-		    <a href="https://migros-engagement.ch/engagement" target="_blank" rel="noopener noreferrer" 
-		    	className={`migros-logo migros-link blend ${wp < 3 || 
-			    mobileNavOpen ? 'show' : 'hide'}`}>
-			    <img src={logoSrcW} alt="Migros Kulturprozent" />
-		    </a>
-		    <Main 
-		    	data={props.data}
-		    	wp={wp}
-		    	setWp={setWp}
-		    	prevWp={prevWp}
-		    	setPrevWp={setPrevWp}
-		    	jumpTo={jumpTo}
-		    	inverted={inverted}
-		    	isIntro={isIntro}
-		    	isMobile={isMobile}
-		    	isTouch={isTouch}
-		    	mobileNavOpen={mobileNavOpen}
-		    	setMobileNavOpen={setMobileNavOpen}
-		    	onExtraWheel={onExtraWheel}
-		    />
-		    <Navi 
-		    	data={props.data}
-					wp={wp}
-					jumpTo={jumpTo}
-					isIntro={isIntro}
-					isMobile={isMobile}
-					mobileNavOpen={mobileNavOpen}
-					onExtraWheel={onExtraWheel}
-		    />
-	    </React.Fragment>}
-	  </div>
-  )
+	return (
+		<div id="app" className={classes}>
+			{isIE ? (
+				<React.Fragment>
+					<h1>Ihr Browser (Internet Explorer) ist leider veraltet.</h1>
+					<h2>Diese Homepage kann so nicht angezeigt werden.</h2>
+					<br />
+					<p>
+						Bitte installieren Sie einen aktuellen Browser wie zB.{" "}
+						<a href="https://www.google.com/chrome/">Google Chrome</a> oder{" "}
+						<a href="https://www.mozilla.org/de/firefox/new/">Firefox</a>.
+					</p>
+					<p>
+						Mehr Informationen darüber wieso man keine veralteten Browser
+						benutzen sollte:{" "}
+						<a href="https://www.browser-update.org/de/update.html">
+							hier klicken
+						</a>
+					</p>
+				</React.Fragment>
+			) : (
+				<React.Fragment>
+					{/*<Three show={isIntro || mobileNavOpen} />*/}
+					<header
+						className={`text-logo front 
+		    	${(!isIntro && !isMobile) || mobileNavOpen ? "show" : "hide"}`}
+					>
+						<div className="row">
+							<span className="left suffix-slash">Sommer</span>
+							<span className="right">25.</span>
+						</div>
+						<div className="row">
+							<span className="left suffix-slash">Nachts</span>
+							<span className="right">Juni</span>
+						</div>
+						<div className="row">
+							<span className="left suffix-slash">Ball</span>
+							<span className="right">2022</span>
+						</div>
+						<div className="row instagram-link">
+							<a
+								href="https://www.instagram.com/sommernachtsballzuerich"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<InstagramIcon className="icon" />
+							</a>
+						</div>
+					</header>
+					<a
+						href="https://migros-engagement.ch/engagement"
+						target="_blank"
+						rel="noopener noreferrer"
+						className={`migros-logo migros-link blend ${
+							wp < 3 || mobileNavOpen ? "show" : "hide"
+						}`}
+					>
+						<img src={logoSrcW} alt="Migros Kulturprozent" />
+					</a>
+					<Main
+						data={props.data}
+						wp={wp}
+						setWp={setWp}
+						prevWp={prevWp}
+						setPrevWp={setPrevWp}
+						jumpTo={jumpTo}
+						inverted={inverted}
+						isIntro={isIntro}
+						isMobile={isMobile}
+						isTouch={isTouch}
+						mobileNavOpen={mobileNavOpen}
+						setMobileNavOpen={setMobileNavOpen}
+						onExtraWheel={onExtraWheel}
+					/>
+					<Navi
+						data={props.data}
+						wp={wp}
+						jumpTo={jumpTo}
+						isIntro={isIntro}
+						isMobile={isMobile}
+						mobileNavOpen={mobileNavOpen}
+						onExtraWheel={onExtraWheel}
+					/>
+				</React.Fragment>
+			)}
+		</div>
+	)
 }
 
 export default App
--- src/components/archivegallery.js
+++ src/components/archivegallery.js
@@ -1,20 +1,22 @@
 import React from "react"
-import { LazyLoadImage, LazyLoadComponent } from "react-lazy-load-image-component"
+import {
+	LazyLoadImage,
+	LazyLoadComponent,
+} from "react-lazy-load-image-component"
 import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined"
 import ArrowBackOutlinedIcon from "@material-ui/icons/ArrowBackOutlined"
 import ArrowForwardOutlinedIcon from "@material-ui/icons/ArrowForwardOutlined"
-import '../styles/archivegallery.scss'
-
-const logoSrcW = require('../assets/migros_w.svg')
+import logoSrcW from "../assets/migros_w.svg"
+import "../styles/archivegallery.scss"
 
 const ArchiveGallery = (props) => {
 	const data = props.data.wpgraphql.archiveGalleries.nodes
 	const [activeGalleryIndex, setActiveGalleryIndex] = React.useState(null)
 	const [activeLightboxEl, setActiveLightboxEl] = React.useState(null)
 
-	if (typeof window !== 'undefined') {
-		document.querySelector('html').classList.add('archive-gallery')
-		document.querySelector('body').classList.add('archive-gallery')
+	if (typeof window !== "undefined") {
+		document.querySelector("html").classList.add("archive-gallery")
+		document.querySelector("body").classList.add("archive-gallery")
 	}
 
 	function onGalleryLinkClick(index) {
@@ -27,94 +29,148 @@ const ArchiveGallery = (props) => {
 	}
 
 	const LightBox = (props) => {
-
 		return (
 			<div id="lightbox">
-				{props.el.mediaType === 'image' && <LazyLoadImage 
-					src={props.el.largeSource} 
-					effect='opacity' 
-					className="lightbox-photo" 
-					threshold={0} 
-					alt={props.el.mediaDetails.meta.caption || props.el.altText} />}
-				{props.el.mimeType === 'video/mp4' && <LazyLoadComponent 
-					className="lightbox-video" 
-					threshold={0}>
-					<span className="video">
-						<video controls>
-							<source src={props.el.mediaItemUrl} type="video/mp4" />
-						</video>
-					</span>
-				</LazyLoadComponent>}
+				{props.el.mediaType === "image" && (
+					<LazyLoadImage
+						src={props.el.largeSource}
+						effect="opacity"
+						className="lightbox-photo"
+						threshold={0}
+						alt={props.el.mediaDetails.meta.caption || props.el.altText}
+					/>
+				)}
+				{props.el.mimeType === "video/mp4" && (
+					<LazyLoadComponent className="lightbox-video" threshold={0}>
+						<span className="video">
+							<video controls>
+								<source src={props.el.mediaItemUrl} type="video/mp4" />
+							</video>
+						</span>
+					</LazyLoadComponent>
+				)}
 				<div className="info">
-					{props.el.mediaDetails.meta?.title && 
-						<label className="meta-title">Titel: {props.el.mediaDetails.meta.title}</label>}
-					{props.el.mediaDetails.meta?.caption && 
-						<label className="meta-caption">Beschreibung: {props.el.mediaDetails.meta.caption}</label>}
-					{props.el.mediaDetails.meta?.copyright && 
-						<label className="meta-copyright">Copyright: {props.el.mediaDetails.meta.copyright}</label>}
+					{props.el.mediaDetails.meta?.title && (
+						<label className="meta-title">
+							Titel: {props.el.mediaDetails.meta.title}
+						</label>
+					)}
+					{props.el.mediaDetails.meta?.caption && (
+						<label className="meta-caption">
+							Beschreibung: {props.el.mediaDetails.meta.caption}
+						</label>
+					)}
+					{props.el.mediaDetails.meta?.copyright && (
+						<label className="meta-copyright">
+							Copyright: {props.el.mediaDetails.meta.copyright}
+						</label>
+					)}
 				</div>
-				{activeLightboxEl > 0 && 
-					<ArrowBackOutlinedIcon className="icon back" fontSize="large" onClick={() => setActiveLightboxEl(activeLightboxEl - 1)} />}
-				{activeLightboxEl + 1 < data[activeGalleryIndex].acfArchiveGallery.media.length && 
-					<ArrowForwardOutlinedIcon className="icon forward" fontSize="large" onClick={() => setActiveLightboxEl(activeLightboxEl + 1)} />}
-				<CloseOutlinedIcon className="icon close" fontSize="large" onClick={() => setActiveLightboxEl(null)} />
-				<label className="count">{(activeLightboxEl + 1) + ' / ' + data[activeGalleryIndex].acfArchiveGallery.media.length}</label>
+				{activeLightboxEl > 0 && (
+					<ArrowBackOutlinedIcon
+						className="icon back"
+						fontSize="large"
+						onClick={() => setActiveLightboxEl(activeLightboxEl - 1)}
+					/>
+				)}
+				{activeLightboxEl + 1 <
+					data[activeGalleryIndex].acfArchiveGallery.media.length && (
+					<ArrowForwardOutlinedIcon
+						className="icon forward"
+						fontSize="large"
+						onClick={() => setActiveLightboxEl(activeLightboxEl + 1)}
+					/>
+				)}
+				<CloseOutlinedIcon
+					className="icon close"
+					fontSize="large"
+					onClick={() => setActiveLightboxEl(null)}
+				/>
+				<label className="count">
+					{activeLightboxEl +
+						1 +
+						" / " +
+						data[activeGalleryIndex].acfArchiveGallery.media.length}
+				</label>
 			</div>
 		)
 	}
 
-  return (
-  	<div id="archive-gallery">
-	    <header className="text-logo">
-	      <div className="row">
-	        <span className="left suffix-slash">Sommer</span>
-	        {/*<span className="right">Abgesagt</span>*/}
-	      </div>
-	      <div className="row">
-	        <span className="left suffix-slash">Nachts</span>
-	        {/*<span className="right">Aufgrund</span>*/}
-	      </div>
-	      <div className="row">
-	        <span className="left suffix-slash">Ball</span>
-	        {/*<span className="right">Coronavirus</span>*/}
-	      </div>
-	    </header>
-	    <img src={logoSrcW} className="migros-logo" alt="Migros Kulturprozent" />
-	    <main id="content">
-	    	<div className="gallery-sticky-top">
-		    	<h1 className="gallery-title">Medien-Archiv</h1>
-		    	{data.map((el, index) => {
-		    		return <span key={index} className={`gallery-link link ${activeGalleryIndex === index ? 'active' : ''}`} 
-		    			onClick={() => onGalleryLinkClick(index)}>{el.title}</span>
-		    	})}
-		    </div>
-	    	{activeGalleryIndex !== null && <article>
-					{data[activeGalleryIndex].acfArchiveGallery.media.map((el, index) => {
-						if (el.mediaType === 'image') return <LazyLoadImage 
-							src={el.mediumSource} 
-							effect='opacity' 
-							width={el.mediaDetails.width} 
-							height={el.mediaDetails.height} 
-							className="gallery-photo" 
-							threshold={0} 
-							onClick={() => onImageClick(index)} 
-							alt={el.mediaDetails.meta.caption || el.altText} />
-						if (el.mimeType === 'video/mp4') return <LazyLoadComponent 
-							className="gallery-video" 
-							threshold={0}>
-							<span className="video">
-								<video controls>
-									<source src={el.mediaItemUrl} type="video/mp4" />
-								</video>
+	return (
+		<div id="archive-gallery">
+			<header className="text-logo">
+				<div className="row">
+					<span className="left suffix-slash">Sommer</span>
+					{/*<span className="right">Abgesagt</span>*/}
+				</div>
+				<div className="row">
+					<span className="left suffix-slash">Nachts</span>
+					{/*<span className="right">Aufgrund</span>*/}
+				</div>
+				<div className="row">
+					<span className="left suffix-slash">Ball</span>
+					{/*<span className="right">Coronavirus</span>*/}
+				</div>
+			</header>
+			<img src={logoSrcW} className="migros-logo" alt="Migros Kulturprozent" />
+			<main id="content">
+				<div className="gallery-sticky-top">
+					<h1 className="gallery-title">Medien-Archiv</h1>
+					{data.map((el, index) => {
+						return (
+							<span
+								key={index}
+								className={`gallery-link link ${
+									activeGalleryIndex === index ? "active" : ""
+								}`}
+								onClick={() => onGalleryLinkClick(index)}
+							>
+								{el.title}
 							</span>
-						</LazyLoadComponent>
+						)
 					})}
-	    	</article>}
-	    </main>
-	    {activeLightboxEl !== null && <LightBox 
-	    	el={data[activeGalleryIndex].acfArchiveGallery.media[activeLightboxEl]} />}
-  	</div>
-  )
+				</div>
+				{activeGalleryIndex !== null && (
+					<article>
+						{data[activeGalleryIndex].acfArchiveGallery.media.map(
+							(el, index) => {
+								if (el.mediaType === "image")
+									return (
+										<LazyLoadImage
+											src={el.mediumSource}
+											effect="opacity"
+											width={el.mediaDetails.width}
+											height={el.mediaDetails.height}
+											className="gallery-photo"
+											threshold={0}
+											onClick={() => onImageClick(index)}
+											alt={el.mediaDetails.meta.caption || el.altText}
+										/>
+									)
+								if (el.mimeType === "video/mp4")
+									return (
+										<LazyLoadComponent className="gallery-video" threshold={0}>
+											<span className="video">
+												<video controls>
+													<source src={el.mediaItemUrl} type="video/mp4" />
+												</video>
+											</span>
+										</LazyLoadComponent>
+									)
+							}
+						)}
+					</article>
+				)}
+			</main>
+			{activeLightboxEl !== null && (
+				<LightBox
+					el={
+						data[activeGalleryIndex].acfArchiveGallery.media[activeLightboxEl]
+					}
+				/>
+			)}
+		</div>
+	)
 }
 
 export default ArchiveGallery
